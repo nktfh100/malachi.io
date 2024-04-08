ad16e188714aa3e47cf01b4d85130065e01ea929
nktfh100 <me@nktfh100.com>
2024-04-08 20:59:13 +0300

    Add vite legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import path from "path";
 import { defineConfig } from "vite";
-
+import legacy from "@vitejs/plugin-legacy";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		legacy({
+			targets: ["defaults", "not IE 11"],
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
