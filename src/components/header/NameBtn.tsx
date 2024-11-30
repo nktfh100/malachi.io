ffc9bac181a83d0fcbdd58aca6da21737db3f0a2
nktfh100 <me@malachi.io>
2024-11-30 22:37:08 +0200

    Small fix

--- src/components/header/NameBtn.tsx
+++ src/components/header/NameBtn.tsx
@@ -2,8 +2,11 @@ import { Link } from "@tanstack/react-router";
 
 export const NameBtn = () => {
   return (
-    <button className="text-4xl font-black bg-gradient-to-r from-ctp-pink to-ctp-mauve bg-clip-text text-transparent hidden sm:block">
-      <Link to="/">Malachi</Link>
-    </button>
+    <Link
+      to="/"
+      className="text-4xl font-black bg-gradient-to-r from-ctp-pink to-ctp-mauve bg-clip-text text-transparent hidden sm:block"
+    >
+      Malachi
+    </Link>
   );
 };
