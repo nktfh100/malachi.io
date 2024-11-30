fc6a3e4ece837d4c942ed17deb688fa13e4a6514
nktfh100 <me@malachi.io>
2024-11-30 22:35:06 +0200

    Make header button clickable

--- src/components/header/NameBtn.tsx
+++ src/components/header/NameBtn.tsx
@@ -1,7 +1,9 @@
+import { Link } from "@tanstack/react-router";
+
 export const NameBtn = () => {
   return (
-    <button className="text-3xl font-black bg-gradient-to-r from-ctp-pink to-ctp-mauve bg-clip-text text-transparent hidden sm:block">
-      Malachi
+    <button className="text-4xl font-black bg-gradient-to-r from-ctp-pink to-ctp-mauve bg-clip-text text-transparent hidden sm:block">
+      <Link to="/">Malachi</Link>
     </button>
   );
 };
