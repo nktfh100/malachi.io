be2d4491d6f6ba369a14de46fc56f1950d06d735
nktfh100 <me@malachi.io>
2025-04-29 22:03:44 +0300

    Update profile picture

--- src/components/home/ProfilePic.tsx
+++ src/components/home/ProfilePic.tsx
@@ -1,24 +1,24 @@
-import { cn } from "@/lib/utils";
-import { motion } from "motion/react";
+import { cn } from '@/lib/utils';
+import { motion } from 'motion/react';
 
 type ProfilePicProps = {
-  className?: string;
-  extraProps?: any;
+	className?: string;
+	extraProps?: any;
 };
 
 export const ProfilePic = ({ extraProps, className }: ProfilePicProps) => {
-  return (
-    <motion.img
-      src="/malachi.png"
-      alt="Malachi"
-      className={cn(
-        "w-44 sm:w-48 md:w-60 lg:w-80 mx-auto rounded-full shadow-ctp-overlay0 hover:shadow-ctp-mauve transition-shadow duration-200",
-        className,
-      )}
-      style={{
-        boxShadow: "0px 0px 20px 3px var(--tw-shadow-color)",
-      }}
-      {...extraProps}
-    />
-  );
+	return (
+		<motion.img
+			src="/malachi.jpg"
+			alt="Malachi"
+			className={cn(
+				'w-44 sm:w-48 md:w-60 lg:w-80 mx-auto rounded-full shadow-ctp-overlay0 hover:shadow-ctp-mauve transition-shadow duration-200',
+				className
+			)}
+			style={{
+				boxShadow: '0px 0px 20px 3px var(--tw-shadow-color)',
+			}}
+			{...extraProps}
+		/>
+	);
 };
